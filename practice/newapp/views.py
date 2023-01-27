ebc3d42b60f6b0fbabb7a37620fe64fe0610446c
Carmine Corbo <carmine.j.corbo@gmail.com>
2023-01-27 14:23:53 -0500

    Post now show up on page.

--- practice/newapp/views.py
+++ practice/newapp/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse, request
 from django.template import loader
+from newapp.models import Post
+
+
 
 # Create your views here.
 def home(request):
-    template = loader.get_template('base.html')
-    return HttpResponse(template.render())
-
+    template = loader.get_template('posts.html')
+    posts = Post.objects.all()
+    context = {
+        'posts':posts
+    }
+    return render(request, 'posts.html' , context)
